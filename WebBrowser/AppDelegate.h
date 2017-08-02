34236084175abb0bf6a8b78667b1d61e95099df9
pradeep gunasekaran <pradeepgunasekaran@pradeeps-macbook-pro.local>
2017-08-02 14:55:33 -0400

    Initial Commit

---
+++ WebBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebBrowser
+//
+//  Created by pradeep gunasekaran on 8/2/17.
+//  Copyright © 2017 pradeep gunasekaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebBrowser
+//
+//  Created by pradeep gunasekaran on 8/2/17.
+//  Copyright © 2017 pradeep gunasekaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
